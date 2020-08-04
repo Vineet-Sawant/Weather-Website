60645108b345409240c38d0ea786e92f241f2471
vineet sawant <vineetswant@gmail.com>
2020-08-04 10:46:40 +0530

    made some changes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // local host or heroku 
 
 const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
